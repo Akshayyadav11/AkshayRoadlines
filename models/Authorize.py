e1ea52f2b575614d784a3d1e9f25a69f760a5c23
Akshay <akshayy976@gmail.com>
2021-11-04 16:25:48 +0530

    checking on refresh token

--- models/Authorize.py
+++ models/Authorize.py
@@ -8,42 +8,77 @@ from flask.globals import session
 import jwt
 
 class Authorize():
+    # def token_required(func):
+    #     @wraps(func)
+    #     def decorated(*args, **kwrags):
+    #         try:
+    #             token=""
+    #             # if 'Authorization' in request.headers:
+    #             #     data = request.headers['Authorization']
+    #             #     token = str.replace(str(data), 'Bearer ', '')
+    #             print("--authorize-session-",session['profile'])
+    #             if 'profile' not in session:
+    #                 print('profile not in session')
+    #                 return redirect('/login')
+    #             # token = request.args.get('token')
+    #             # token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NywiZXhwIjoxNjM1NzUwMjk0fQ.UbVtIq3ftwFgj4qig22kE9CFH0KhqG2f2A7ygnUxxhE"
+    #                 # print("---------token-",token)
+    #             # if not token:
+    #             #     return jsonify({"message":'Token is missing'},500)
+    #             try:
+    #                 payload = jwt.decode(session['profile']['access_token'], app.config['SECRET_KEY'],algorithms=["HS256"])
+    #                 print("payload-----",payload)
+    #             except:
+    #                 return jsonify({"Invalid token"})
+    #         except Exception as exc:
+    #             print('------profile---exception',exc)
+    #             return jsonify({"message":'Token is Expired/Invalid'},500)
+    #             # raise (str(exc))
+    #         # return payload 
+            
+    #         return func(*args, **kwrags)
+    #     return decorated
+
     def token_required(func):
         @wraps(func)
         def decorated(*args, **kwrags):
             try:
                 token=""
-                # if 'Authorization' in request.headers:
-                #     data = request.headers['Authorization']
-                #     token = str.replace(str(data), 'Bearer ', '')
-                print("--authorize-session-",session['profile'])
-                if 'profile' not in session:
-                    print('profile not in session')
-                    return redirect('/login')
-                # token = request.args.get('token')
-                # token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6NywiZXhwIjoxNjM1NzUwMjk0fQ.UbVtIq3ftwFgj4qig22kE9CFH0KhqG2f2A7ygnUxxhE"
-                    # print("---------token-",token)
-                # if not token:
-                #     return jsonify({"message":'Token is missing'},500)
+                if 'Authorization' in request.headers:
+                    data = request.headers['Authorization']
+                    token = str.replace(str(data), 'Bearer ', '')
+                    print("token---",token)
+                if not token:
+                    return make_response(
+                        'Token is missing !!',
+                        401,
+                        {'WWW-Authenticate' : 'Basic realm ="Login Required !!"'}
+                    ) 
                 try:
-                    payload = jwt.decode(session['profile']['access_token'], app.config['SECRET_KEY'],algorithms=["HS256"])
+                    payload = jwt.decode(token, app.config['SECRET_KEY'],algorithms=["HS256"])
                     print("payload-----",payload)
                 except:
                     return jsonify({"Invalid token"})
             except Exception as exc:
-                print('------profile---exception',exc)
-                return jsonify({"message":'Token is Expired/Invalid'},500)
+                
+                return make_response(
+                        'The access token expired/Invalid !!',
+                        401,
+                        {'WWW-Authenticate' : 'Basic realm ="Login Required !!"'}
+                    ) 
+                    
+                # return jsonify({"message":'Token is Expired/Invalid'},500)
                 # raise (str(exc))
-            # return payload 
+
+            
             
             return func(*args, **kwrags)
         return decorated
-
 
     def create_access_token(response_data):
         access_token = jwt.encode({'userId':response_data['id'],'emailId':response_data['email'], 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
         return access_token
     
     def create_refresh_token(response_data):
-        refresh_token = jwt.encode({'userId':response_data['id'],'emailId':response_data['email'], 'grant_type':'refresh', 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config['SECRET_KEY'])
+        refresh_token = jwt.encode({'userId':response_data['id'],'emailId':response_data['email'], 'grant_type':'refresh'},app.config['SECRET_KEY'])
         return refresh_token
--- services/Users.py
+++ services/Users.py
@@ -19,6 +19,70 @@ users = Blueprint("users", __name__)
 
 @users.route('/')
 @users.route('/login', methods =['GET', 'POST'])
+# def login():
+#     try:     
+#         msg = '' 
+#         response_data = ''           
+#         if request.method == 'POST' and 'emailId' in request.form and 'password' in request.form:
+    
+#             emailId = request.form['emailId']
+#             password = request.form['password']
+#             # print("---",emailId,password)
+#             loginDetails =  Users()
+#             response_data = loginDetails.login(emailId)
+#             if response_data is None:
+#                 return make_response(
+#                         'Could not verify!!',
+#                         401,
+#                         {'WWW-Authenticate' : 'Basic realm ="Login Required !!"'}
+#                     ) 
+            
+#             if check_password_hash(response_data['password'],password):
+#                 # print("-------in inf-------")
+#                 access_token = Authorize.create_access_token(response_data)
+
+               
+#                 session['jwt_payload'] = response_data
+#                 session['profile'] = {
+#                         'access_token' : access_token,
+#                         'userId':response_data['id'],
+#                         'emailId':response_data['email'],
+#                         'message':'Logged in successfully'
+#                 }
+#                 # print(session)
+#                 return redirect('/index')
+
+#                 # refresh_token = Authorize.create_refresh_token(response_data)
+
+#                 # return make_response(jsonify({
+#                 #                     'access_token' : access_token,
+#                 #                     'refresh_token':refresh_token,
+#                 #                     'userId':response_data['id'],
+#                 #                     'emailId':response_data['email'],
+#                 #                     'message':'Logged in successfully'
+#                 #                     }), 201)
+            
+#             #print("check_password_hash--else-")
+#             return make_response(
+#                     'Could not verify passoword !!',
+#                     401,
+#                     {'WWW-Authenticate' : 'Basic realm ="Login Required !!"'}
+#                 ) 
+
+#         #     if response_data  == "Logged in successfully !":
+#         #         return render_template('index.html', msg = response_data)
+#         #     else:
+#         #         return render_template('login.html', msg = response_data)
+               
+#         else:
+            
+#             return render_template('login.html')
+        
+#     except Exception as e: 
+#         print(e)       
+#         return jsonify({"error": str(e)}), 500   
+
+
 def login():
     try:     
         msg = '' 
@@ -36,31 +100,27 @@ def login():
                         401,
                         {'WWW-Authenticate' : 'Basic realm ="Login Required !!"'}
                     ) 
-            
+
+            session['userId'] = response_data['id']
+            session['emailId'] = response_data['email']
+
             if check_password_hash(response_data['password'],password):
                 # print("-------in inf-------")
+
                 access_token = Authorize.create_access_token(response_data)
 
                
-                session['jwt_payload'] = response_data
-                session['profile'] = {
-                        'access_token' : access_token,
-                        'userId':response_data['id'],
-                        'emailId':response_data['email'],
-                        'message':'Logged in successfully'
-                }
-                # print(session)
-                return redirect('/index')
+                refresh_token = Authorize.create_refresh_token(response_data)
 
-                # refresh_token = Authorize.create_refresh_token(response_data)
-
-                # return make_response(jsonify({
-                #                     'access_token' : access_token,
-                #                     'refresh_token':refresh_token,
-                #                     'userId':response_data['id'],
-                #                     'emailId':response_data['email'],
-                #                     'message':'Logged in successfully'
-                #                     }), 201)
+                return make_response(jsonify({
+                                    'access_token' : access_token,
+                                    'refresh_token':refresh_token,
+                                    'userId':response_data['id'],
+                                    'emailId':response_data['email'],
+                                    'token_type':'bearer',
+                                    "expires": 1800,
+                                    'message':'Logged in successfully'
+                                    }), 201)
             
             #print("check_password_hash--else-")
             return make_response(
@@ -68,12 +128,7 @@ def login():
                     401,
                     {'WWW-Authenticate' : 'Basic realm ="Login Required !!"'}
                 ) 
-
-        #     if response_data  == "Logged in successfully !":
-        #         return render_template('index.html', msg = response_data)
-        #     else:
-        #         return render_template('login.html', msg = response_data)
-               
+              
         else:
             
             return render_template('login.html')
@@ -81,7 +136,6 @@ def login():
     except Exception as e: 
         print(e)       
         return jsonify({"error": str(e)}), 500   
-
 
 @users.route('/protected', methods =['GET', 'POST'])
 @Authorize.token_required
@@ -92,3 +146,24 @@ def protected():
 @users.route('/unprotected', methods =['GET', 'POST'])
 def unprotected():
     return jsonify({"message": str("unprotected")}), 200 
+
+
+@users.route('/refresh/token',methods =['GET', 'POST'])
+def refresh_token():
+    userId = session['userId'] 
+    emailId = session['emailId']
+    data = {
+        'id' : userId,
+        'email':emailId
+    }
+    access_token = Authorize.create_access_token(data)
+    refresh_token = Authorize.create_refresh_token(data)
+    return make_response(jsonify({
+                                    'access_token' : access_token,
+                                    'refresh_token':refresh_token,
+                                    'userId':userId,
+                                    'emailId':emailId,
+                                    'token_type':'bearer',
+                                    "expires": 1800
+                                    
+                                    }), 201)
